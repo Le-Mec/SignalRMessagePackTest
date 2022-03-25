82ba329af7553a972de1eed81195bc867350d30c
Eli Taylor <eli.taylor@asirobots.com>
2022-03-25 09:52:50 -0600

    Initial commit without messagepack

---
+++ SignalRServer/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace SignalRServer
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			CreateHostBuilder(args).Build().Run();
+		}
+
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+			Host.CreateDefaultBuilder(args)
+				.ConfigureWebHostDefaults(webBuilder =>
+				{
+					webBuilder.UseStartup<Startup>();
+					webBuilder.UseUrls("https://localhost:59639");
+				});
+	}
+}
